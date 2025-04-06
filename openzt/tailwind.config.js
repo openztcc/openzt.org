282a33f6af5277c8cd20a8f0f57e32b3cc591684
Eric Galvan <lericgalvan@outlook.com>
2025-04-06 16:09:24 -0700

    Define custom openzt colors

--- openzt/tailwind.config.js
+++ openzt/tailwind.config.js
@@ -1,8 +1,23 @@
 
-content: [
-  "./layouts/**/*.html",
-  "./content/**/*.md",
-  "./assets/**/*.js",
-  "./public/**/*.html",
-]
-  
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: '#BEE5B0',
+          DEFAULT: '#93B885',
+          dark: '#77956C',
+        },
+        accent: {
+          DEFAULT: '#FED284',
+        }
+      },
+    },
+  },
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.md",
+    "./assets/**/*.js",
+    "./public/**/*.html",
+  ]
+};
